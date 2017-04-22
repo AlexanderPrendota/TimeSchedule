6a024484bead403e40ed876289663ae7153375ae
aleksandrprendota <prendota@mail.ru>
2017-04-22 12:27:05 +0300

    go

---
+++ src/main/java/net/tableschedule/jsf/bean/MQListener.java
@@ -0,0 +1,56 @@
+package net.tableschedule.jsf.bean;
+
+import com.rabbitmq.client.*;
+import lombok.AllArgsConstructor;
+import org.omnifaces.util.Ajax;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+
+/**
+ * Created by aleksandrprendota on 21.04.17.
+ */
+@SessionScoped
+@AllArgsConstructor
+@ManagedBean(name = "mqlistener", eager = true)
+public class MQListener {
+    private final static String QUEUE_NAME = "mylittlequeue";
+
+    public void startListener() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+                if (message.contains("update")){
+                    // 1 way:
+                    // ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+                    // ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+
+                    // 2 way:
+                    //Ajax.update(":maintable");
+
+                    // another way
+                    String page = "home.xhtml";
+                    FacesContext.getCurrentInstance().getExternalContext().redirect(page);
+                }
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+
+    public void restart(){
+
+    }
+}
---
+++ src/main/java/net/tableschedule/jsf/bean/TimeScheduleClient.java
@@ -0,0 +1,57 @@
+package net.tableschedule.jsf.bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by aleksandrprendota on 18.04.17.
+ */
+
+@Data
+@SessionScoped
+@NoArgsConstructor
+@AllArgsConstructor
+@ManagedBean(name = "schedule", eager = true)
+public class TimeScheduleClient {
+
+    public List<TimeSchedule> timeSchedules;
+
+    @SuppressWarnings({"unchecked", "unused"})
+    public void getContent() throws Exception{
+
+
+        Client client = Client.create();
+        WebResource webResource = client.resource("http://localhost:8080/schedule/todays");
+        ObjectMapper mapper = new ObjectMapper();
+        ClientResponse response = webResource
+                .accept(MediaType.APPLICATION_JSON)
+                .get(ClientResponse.class);
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + response.getStatus());
+        }
+        String output = response.getEntity(String.class);
+
+        try {
+            timeSchedules = mapper.readValue(output, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<TimeSchedule> getTimeSchedules(){
+        return timeSchedules;
+    }
+
+}
